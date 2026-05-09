8852ed84eecd79702ab2690c52c0e3903fb8adeb
Navathakamaraj <navathakamaraj2006@gmail.com>
2026-05-09 14:57:44 +0530

    Delete adminpanel directory

--- adminpanel/src/main/java/pharama/example/adminpanel/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package pharama.example.adminpanel.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pharama.example.adminpanel.model.CategoryMaster;
-
-public interface CategoryRepository extends JpaRepository<CategoryMaster, Long> {
-}
--- adminpanel/src/main/java/pharama/example/adminpanel/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package pharama.example.adminpanel.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pharama.example.adminpanel.model.ProductMaster;
-
-@Repository
-public interface ProductRepository extends JpaRepository<ProductMaster, Long> {}
